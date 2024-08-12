4cf54aa5ab78a8f3e91bf173c62d10278106c33a
Sam Wen <samswen@gmail.com>
2024-08-12 13:47:02 +0000

    update

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const Base = require('./base');
+const { 
+    setPassphrase, 
+    getPassphrase, 
+    encryptString, 
+    decryptString 
+} = require('./');
 
 (async () => {
-    // setPassphrase('x5fki+cuEvwXC6jZrMUX5TZT4K9fvFdTJHyr4dOOZiYE0wJ+EyL3F8k');
-    // console.log(getPassphrase());
-    // const encrypted = await encryptString('Hello, World!');
-    // console.log(encrypted);
-    // const decrypted = await decryptString(encrypted);
-    // console.log(decrypted);
-    // const instance = new Base();
-    // const buffer = instance.str2ab('test');
-    // console.log(buffer.toString());
-    console.log(window);
+    setPassphrase('x5fki+cuEvwXC6jZrMUX5TZT4K9fvFdTJHyr4dOOZiYE0wJ+EyL3F8k');
+    console.log(getPassphrase());
+    const encrypted = await encryptString('Hello, World!');
+    console.log(encrypted);
+    const decrypted = await decryptString(encrypted);
+    console.log(decrypted);
 })();
